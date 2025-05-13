d7a1c6e4a0795e06da0c65fd464ca646c4a01719
gaba23 <gabrielalvesacardoso@gmail.com>
2025-05-12 23:23:50 -0300

    Troca BrowserRouter por HashRouter para melhor compatibilidade

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import { LoginPage } from './pages/LoginPage.tsx';
 import { RegisterPage } from './pages/RegisterPage.tsx';
 import { HomePage } from './pages/HomePage.tsx';
@@ -9,12 +9,14 @@ import './App.css';
 function App() {
   return (
     <AuthProvider>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/" element={<LoginPage />} />
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+      </HashRouter>
     </AuthProvider>
   );
 }
